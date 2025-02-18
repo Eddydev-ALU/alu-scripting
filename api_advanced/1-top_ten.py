cffb8bc437643aeeb768e979211adec8e02be8d7
Eddydev-ALU <e.irasetsa@alustudent.com>
2025-02-18 00:58:25 -0800

    top 10 hotspots

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,10 +6,7 @@ import requests
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {
-        "User-Agent": "python:reddit-api-client:v1.0 (by /u/yourusername)"
-    }
-    
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
